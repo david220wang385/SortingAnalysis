f6612315c61440a7bf42ff50b036a2adefcddf46
David W <david220wang385@gmail.com>
2019-07-17 00:50:03 -0400

    Added list generation and template for sorting algos

--- SortingAnalysis/SortingAnalysis.cpp
+++ SortingAnalysis/SortingAnalysis.cpp
@@ -6,8 +6,13 @@
 // References
 // https://stackoverflow.com/questions/6926433/how-to-shuffle-a-stdvector
 // https://stackoverflow.com/questions/8877448/how-do-i-reverse-a-c-vector
+// https://stackoverflow.com/questions/12231166/timing-algorithm-clock-vs-time-in-c
 
 using namespace std;
+
+void BubbleSort(vector<int>& list);
+void MergeSort(vector<int>& list);
+void QuickSort(vector<int>& list);
 
 int main() {
 	
@@ -19,12 +24,29 @@ int main() {
 	vector<int> random_order;
 		
 	for (int i = 0; i < lengths.size(); i++) {
-		
+
+		ascend_order.clear();
+		descend_order.clear();
+		random_order.clear();
+
 		ascend_order.reserve(lengths[i]);
 		descend_order.reserve(lengths[i]);
 		random_order.reserve(lengths[i]);
+
+		for (int j = 0; j < lengths[i]; j++) {
+			ascend_order.push_back(j);
+		}
+		random_order = ascend_order;
+		std::shuffle(random_order.begin(), random_order.end(), rng);
+		descend_order = ascend_order;
+		std::reverse(descend_order.begin(), descend_order.end());
+
+		// DEBUG FIXME
+		//for (auto it = random_order.begin(); it != random_order.end(); it++) {
+		//	cout << *it << " ";
+		//}
+
+		return 0;
 	}
 
-	vector<int> temp;
-	std::shuffle(temp.begin(), temp.end(), rng);
 }
