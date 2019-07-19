fa5f6e61837961bbfcc52e56bda7f213b7c66313
David W <david220wang385@gmail.com>
2019-07-19 10:32:04 -0400

    Swapped out QuickSort for InsertionSort b/c stack overflow errors

--- SortingAnalysis/SortingAnalysis.cpp
+++ SortingAnalysis/SortingAnalysis.cpp
@@ -2,26 +2,31 @@
 #include <vector>
 #include <algorithm>
 #include <random>
+#include <chrono>
 
 // References
 // https://stackoverflow.com/questions/6926433/how-to-shuffle-a-stdvector
 // https://stackoverflow.com/questions/8877448/how-do-i-reverse-a-c-vector
 // https://stackoverflow.com/questions/421573/best-way-to-extract-a-subvector-from-a-vector
 // https://stackoverflow.com/questions/12231166/timing-algorithm-clock-vs-time-in-c
+// https://stackoverflow.com/questions/15092504/how-to-time-a-function-in-milliseconds-without-boosttimer
 
 using namespace std;
+using namespace std::chrono;
 
+// Prototypes
 void BubbleSort(vector<int>& list);
 void swap(int* a, int* b);
 vector<int> MergeSort(vector<int> list);
 vector<int> Merge(vector<int>& leftSub, vector<int>& rightSub);
 void QuickSort(vector<int>& list, int lowIndex, int highIndex);
 int Partition(vector<int>& list, int lowIndex, int highIndex);
+void InsertionSort(vector<int>& list);
 
 int main() {
 	
 	auto rng = std::default_random_engine{};
-	vector<int> lengths = { 500, 10000, 20000, 50000 };
+	vector<int> lengths = { 50000, 10000, 20000, 50000 };
 
 	vector<int> ascend_order;
 	vector<int> descend_order;
@@ -33,6 +38,7 @@ int main() {
 		descend_order.clear();
 		random_order.clear();
 
+		// Creating the lists
 		ascend_order.reserve(lengths[i]);
 		descend_order.reserve(lengths[i]);
 		random_order.reserve(lengths[i]);
@@ -45,16 +51,15 @@ int main() {
 		descend_order = ascend_order;
 		std::reverse(descend_order.begin(), descend_order.end());
 
-		// DEBUG FIXME
-		for (auto it = random_order.begin(); it != random_order.end(); it++) {
-			cout << *it << " ";
-		}
-		BubbleSort(ascend_order);
-		QuickSort(random_order, 0, random_order.size());
+		// Timing sorting algorithms
+		auto start = std::chrono::steady_clock::now();
+		InsertionSort(random_order);
+		auto end = std::chrono::steady_clock::now();
+		auto duration = duration_cast<milliseconds>(end - start);
+
+		std::cout << std::endl << duration.count() << " miliseconds" << std::endl;
 		cout << endl;
-		for (auto it = random_order.begin(); it != random_order.end(); it++) {
-			cout << *it << " ";
-		}
+
 
 		return 0;
 	}
@@ -128,12 +133,29 @@ vector<int> Merge(vector<int>& listA, vector<int>& listB) {
 	return sorted;
 }
 
+void InsertionSort(vector<int>& list) {
+	for (unsigned int i = 0; i < list.size(); i++) {
+		int j = i;
+		while (j > 0 && list[j - 1] > list[j]) {
+			swap(list[j], list[j - 1]);
+			j--;
+		}
+	}
+}
+
+// Cries in stack overflow
 void QuickSort(vector<int>& list, int lowIndex, int highIndex) {
 	
 	if (lowIndex < highIndex) {
 		int pivotIndex = Partition(list, lowIndex, highIndex);
-		QuickSort(list, lowIndex, pivotIndex);
-		QuickSort(list, pivotIndex+1, highIndex);
+		if (pivotIndex - lowIndex < highIndex - pivotIndex) {
+			QuickSort(list, lowIndex, pivotIndex);
+			QuickSort(list, pivotIndex + 1, highIndex);
+		}
+		else {
+			QuickSort(list, pivotIndex + 1, highIndex);
+			QuickSort(list, lowIndex, pivotIndex);
+		}
 	}
 }
 
@@ -141,7 +163,7 @@ int Partition(vector<int>& list, int lowIndex, int highIndex) {
 	int pivot = list[lowIndex];
 	int leftFence = lowIndex;
 
-	for (unsigned int i = lowIndex + 1; i < highIndex; i++) {
+	for (int i = lowIndex + 1; i < highIndex; i++) {
 		if (list[i] < pivot) {
 			swap(list[i], list[leftFence]);
 			leftFence++;
